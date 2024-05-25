d81b89bb51c1b1f837d7c037f3deb61947c09bec
yhx <2045399856@qq.com>
2024-05-26 00:18:14 +0800

    添加 loadEnv依赖

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: yhx 2045399856@qq.com
  * @Date: 2024-05-23 09:05:27
- * @LastEditTime: 2024-05-26 00:16:02
+ * @LastEditTime: 2024-05-26 00:17:48
  * @FilePath: \vue-template-export-word\vite.config.js
  * @Description:
  *
@@ -11,7 +11,7 @@ import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode, command }) => {
