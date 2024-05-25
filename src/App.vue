856329c4a8da5df4bd470da9acc2f4f44473f017
yhx <2045399856@qq.com>
2024-05-25 22:33:19 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
+<!--
+ * @Author: yhx 2045399856@qq.com
+ * @Date: 2024-05-23 09:05:27
+ * @LastEditTime: 2024-05-25 22:25:39
+ * @FilePath: \vue-template-export-word\src\App.vue
+ * @Description: 
+ * 
+-->
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+console.log(import.meta.env.VITE_APP_BASE_API);
 </script>
 
 <template>
--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,29 @@
 /*
  * @Author: yhx 2045399856@qq.com
  * @Date: 2024-05-23 09:05:27
- * @LastEditTime: 2024-05-25 22:22:18
+ * @LastEditTime: 2024-05-25 22:32:45
  * @FilePath: \vue-template-export-word\vite.config.js
  * @Description:
  *
  */
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
-const baseUrl = import.meta.env.VITE_APP_BASE_API
+import { loadEnv, defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: baseUrl,
-  plugins: [vue(), vueJsx(), VueDevTools()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode, command }) => {
+  const env = loadEnv(mode, process.cwd())
+  const { VITE_APP_BASE_API } = env
+  return {
+    base: VITE_APP_BASE_API,
+    plugins: [vue(), vueJsx(), VueDevTools()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
 })
