69cbaa68632fba60a99555a10c8670088168228a
yhx <2045399856@qq.com>
2024-05-25 23:07:12 +0800

    修改

--- vite.config.js
+++ vite.config.js
@@ -11,14 +11,11 @@ import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
-import { loadEnv, defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode, command }) => {
-  const env = loadEnv(mode, process.cwd())
-  const { VITE_APP_BASE_API } = env
   return {
-    base: VITE_APP_BASE_API,
     plugins: [vue(), vueJsx(), VueDevTools()],
     resolve: {
       alias: {
